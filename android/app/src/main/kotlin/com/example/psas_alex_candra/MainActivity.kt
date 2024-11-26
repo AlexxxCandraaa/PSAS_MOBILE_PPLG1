3ea4350bab71daf460ceac8cec1d319e6676a90d
AlexxxCandraaa <alexmarhas59@gmail.com>
2024-11-26 11:29:27 +0700

    commit psas

---
+++ android/app/src/main/kotlin/com/example/psas_alex_candra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.psas_alex_candra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/HomePage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static String tag = 'home-page';
+
+  @override
+  Widget build(BuildContext context) {
+    final alucard = Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CircleAvatar(
+          radius: 72.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/alucard.jpg'),
+        ),
+      ),
+    );
+
+    final welcome = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Welcome Alex Candra!',
+        style: TextStyle(fontSize: 28.0, color: Colors.white),
+      ),
+    );
+
+    final lorem = Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Halo!, Selamat Datang Di Website Alex Candra, Website Ini Masih Dalam Tahap Pengembangan, Jadi Mohon Tunggu Update Kedepannya Yaa!.',
+        style: TextStyle(fontSize: 16.0, color: Colors.white),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 255, 0, 0),
+          Colors.lightBlueAccent,
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[alucard, welcome, lorem],
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
